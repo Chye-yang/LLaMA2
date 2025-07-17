5ef8d2d3e831704d4e34e9273ef0057e8112bd7d
chye-yang <chye.y000@gmail.com>
2025-07-18 02:24:32 +0800

    +lora

--- download/datareduce.py
+++ download/datareduce.py
@@ -13,22 +13,22 @@ output_pretrain_data = 'seq_monkey_datawhale.jsonl'
 sft_data = './download/sft_data/train_3.5M_CN.json'
 output_sft_data = 'BelleGroup_sft.jsonl'
 
-# # 1 处理预训练数据
-# def split_text(text, chunk_size=512):
-#     """将文本按指定长度切分成块"""
-#     return [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
+# 1 处理预训练数据
+def split_text(text, chunk_size=512):
+    """将文本按指定长度切分成块"""
+    return [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
 
-# with open(output_pretrain_data, 'w', encoding='utf-8') as pretrain:  # 使用 'w' 模式清空文件
-#     with open(pretrain_data, 'r', encoding='utf-8') as f:
-#         data = f.readlines()
-#         for i, line in tqdm(enumerate(data), total=min(DATA_LIMIT, len(data)), desc=f"Processing lines in {pretrain_data}", leave=False):  # 添加行级别的进度条
-#             if i >= DATA_LIMIT:
-#                 break  # 达到限制数量后停止处理
-#             line = json.loads(line)
-#             text = line['text']
-#             chunks = split_text(text)
-#             for chunk in chunks:
-#                 pretrain.write(json.dumps({'text': chunk}, ensure_ascii=False) + '\n')
+with open(output_pretrain_data, 'w', encoding='utf-8') as pretrain:  # 使用 'w' 模式清空文件
+    with open(pretrain_data, 'r', encoding='utf-8') as f:
+        data = f.readlines()
+        for i, line in tqdm(enumerate(data), total=min(DATA_LIMIT, len(data)), desc=f"Processing lines in {pretrain_data}", leave=False):  # 添加行级别的进度条
+            if i >= DATA_LIMIT:
+                break  # 达到限制数量后停止处理
+            line = json.loads(line)
+            text = line['text']
+            chunks = split_text(text)
+            for chunk in chunks:
+                pretrain.write(json.dumps({'text': chunk}, ensure_ascii=False) + '\n')
 
 # # 2 处理SFT数据
 def convert_message(data):
